91678965e31e0e5a6dbd745106ee4cf9dd02e0c3
Louis <107303182+0x0Louis@users.noreply.github.com>
2023-04-22 00:26:35 +0200

    Add liq function (#10)
    * rename misleading function
    
    * add amounts and liq function
    
    * initialize arrays

--- src/periphery/LiquidityAmounts.sol
+++ src/periphery/LiquidityAmounts.sol
@@ -65,21 +65,59 @@ library LiquidityAmounts {
         }
     }
 
-    /// @notice Return the ids and liquidities of an user
+    /// @notice Return the balance of an user for a given list of ids
     /// @dev The caller needs to ensure that the ids are unique, if not, the result will be wrong.
     /// @param user The address of the user
     /// @param ids the list of ids where the user have liquidity
     /// @param LBPair The address of the LBPair
-    /// @return liquidities the list of amount of liquidity of the user
-    function getLiquiditiesOf(address user, uint256[] memory ids, address LBPair)
+    /// @return balances the balances of the user for each id
+    function getBalanceOf(address user, uint256[] memory ids, address LBPair)
         internal
         view
-        returns (uint256[] memory liquidities)
+        returns (uint256[] memory balances)
     {
-        liquidities = new uint256[](ids.length);
+        balances = new uint256[](ids.length);
 
         for (uint256 i; i < ids.length; ++i) {
-            liquidities[i] = ILBToken(LBPair).balanceOf(user, ids[i].safe24());
+            balances[i] = ILBToken(LBPair).balanceOf(user, ids[i].safe24());
+        }
+    }
+
+    /// @notice Return the liquidity amounts and the amounts of token in each bin for a given list of ids
+    /// @dev The caller needs to ensure that the ids are unique, if not, the result will be wrong.
+    /// @param user The address of the user
+    /// @param ids the list of ids where the user have liquidity
+    /// @param LBPair The address of the LBPair
+    /// @return amountsX the amounts of token X of the user for each id
+    /// @return amountsY the amounts of token Y of the user for each id
+    /// @return liquidities the liquidity amounts of the user for each id
+    function getAmountsAndLiquiditiesOf(address user, uint256[] memory ids, address LBPair)
+        internal
+        view
+        returns (uint256[] memory amountsX, uint256[] memory amountsY, uint256[] memory liquidities)
+    {
+        liquidities = new uint256[](ids.length);
+        amountsX = new uint256[](ids.length);
+        amountsY = new uint256[](ids.length);
+
+        uint16 binStep = ILBPair(LBPair).getBinStep();
+
+        for (uint256 i; i < ids.length; ++i) {
+            uint24 id = ids[i].safe24();
+
+            uint256 liquidity = ILBToken(LBPair).balanceOf(user, id);
+            (uint256 binReserveX, uint256 binReserveY) = ILBPair(LBPair).getBin(id);
+            uint256 totalSupply = ILBToken(LBPair).totalSupply(id);
+
+            uint256 amountX = liquidity.mulDivRoundDown(binReserveX, totalSupply);
+            uint256 amountY = liquidity.mulDivRoundDown(binReserveY, totalSupply);
+
+            uint256 price = PriceHelper.getPriceFromId(id, binStep);
+
+            amountsX[i] = amountX;
+            amountsY[i] = amountY;
+
+            liquidities[i] = price.mulShiftRoundDown(amountX, Constants.SCALE_OFFSET) + amountY;
         }
     }
 
